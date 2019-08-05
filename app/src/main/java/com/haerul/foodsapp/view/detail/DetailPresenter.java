67fec8d9d2975e1fcbffb3a977f0b982ee9907fe
Daniel Oliveira Rangel <contatomarketingcode@gmail.com>
2019-08-05 14:36:59 -0300

    Commit inicial

---
+++ app/src/main/java/com/haerul/foodsapp/view/detail/DetailPresenter.java
@@ -0,0 +1,41 @@
+package com.haerul.foodsapp.view.detail;
+
+import android.support.annotation.NonNull;
+
+import com.haerul.foodsapp.Utils;
+import com.haerul.foodsapp.model.Meals;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailPresenter {
+    private DetailView view;
+
+    public DetailPresenter(DetailView view) {
+        this.view = view;
+    }
+
+    void getMealById(String mealName) {
+
+        view.showLoading ( );
+
+        Utils.getApi ( ).getMealByName ( mealName ).enqueue ( new Callback<Meals> ( ) {
+            @Override
+            public void onResponse(Call<Meals> call , Response<Meals> response) {
+                view.hideLoading ( );
+                if ( response.isSuccessful ( ) && response.body ( ) != null ) {
+                    view.setMeal ( response.body ( ).getMeals ( ).get ( 0 ) );
+                } else {
+                    view.onErrorLoading ( response.message ( ) );
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Meals> call , Throwable t) {
+                view.hideLoading ( );
+                view.onErrorLoading ( t.getLocalizedMessage ( ) );
+            }
+        } );
+    }
+}
